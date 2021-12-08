60a3e8dfdf6fd03048d16eb3c23ba211b90958ab
arabelhousseyn <potency.football@gmail.com>
2021-12-08 16:30:25 +0000

    change in user model

--- database/migrations/2021_12_08_162626_create_user_phones_table.php
+++ database/migrations/2021_12_08_162626_create_user_phones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserProfilesTable extends Migration
+class CreateUserPhonesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUserProfilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_profiles', function (Blueprint $table) {
+        Schema::create('user_phones', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->string('username')->unique();
-            $table->string('fname');
-            $table->string('lname');
-            $table->tinyInteger('sexe');
-            $table->date('dob');
-            $table->string('picture');
+            $table->string('phone');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -34,6 +29,6 @@ class CreateUserProfilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_profiles');
+        Schema::dropIfExists('user_phones');
     }
 }
