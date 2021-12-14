55b0d5a8a1d420d6c987761583e542aaba9261cb
arabelhousseyn <potency.football@gmail.com>
2021-12-14 08:30:14 +0000

    create BrandProduct model

---
+++ app/Models/BrandProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BrandProduct extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'brand_id',
+        'product_id'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
