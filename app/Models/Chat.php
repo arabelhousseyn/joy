c745ebc4cfd10620c1d77b1c6696eb826d05de4d
arabelhousseyn <potency.football@gmail.com>
2021-12-16 15:37:19 +0000

    crate Chat.php model

---
+++ app/Models/Chat.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Chat extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_title',
+        'product_image',
+        'buyer_id'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
---
+++ database/migrations/2021_12_16_153254_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_title');
+            $table->string('product_image');
+            $table->unsignedBigInteger('buyer_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
