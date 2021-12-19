095d39b781a8164b1ad904410efeef229ac3122b
arabelhousseyn <potency.football@gmail.com>
2021-12-19 14:22:56 +0000

    add relations user for cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -20,4 +20,9 @@ class Cart extends Model
         'updated_at',
         'deleted_at'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
 }
