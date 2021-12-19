3eb701c8aa49b9d127d0bd3eb42c2e2facfde2a0
arabelhousseyn <potency.football@gmail.com>
2021-12-19 13:13:24 +0000

    add relations user and product for feedback model

--- app/Models/Feddback.php
+++ app/Models/Feddback.php
@@ -23,4 +23,14 @@ class Feddback extends Model
         'deleted_at'
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
 }
