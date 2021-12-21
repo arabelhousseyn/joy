4e321ee150e7185bf85ff56b8b785484d2b084bf
arabelhousseyn <potency.football@gmail.com>
2021-12-21 08:31:42 +0000

    add order status

---
+++ database/migrations/2021_12_21_082800_create_order_status_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_status', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->enum('status',['pending','confirmed','cancelled','refunded','shipped']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_status');
+    }
+}
