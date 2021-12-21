b16972f2a23bc46e5ec5298cb3f90d43edfa6dba
arabelhousseyn <potency.football@gmail.com>
2021-12-21 12:34:10 +0000

    create UserGift model

---
+++ database/migrations/2021_12_21_123148_create_user_gifts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserGiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_gifts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('gift_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name_gift');
+            $table->longText('description');
+            $table->double('price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_gifts');
+    }
+}
