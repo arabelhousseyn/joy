bad270dc15a1abead1596bcd4f6759ce51568e5c
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-02-15 12:12:13 +0000

    scale from mysql database to mongodb database

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 
 class Country extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $collection = 'countries';
 
     protected $fillable = [
         'name',
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,32 +40,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Admin::factory(1)->create();
-        User::factory(10)->create();
+//        Admin::factory(1)->create();
+//        User::factory(10)->create();
         Country::factory(1)->create();
-        Province::factory(1)->create();
-        UserAddress::factory(10)->create();
-        UserBillingAddress::factory(1)->create();
-        UserOnline::factory(20)->create();
-        UserPayment::factory(20)->create();
-        UserPhone::factory(10)->create();
-        UserProfile::factory(1)->create();
-        UserProfilePicture::factory(1)->create();
-        UserProvince::factory(1)->create();
-        Category::factory(1)->create();
-        Plan::factory(1)->create();
-        RequestOpenBrand::factory(1)->create();
-        RequestOpenBrandPayment::factory(1)->create();
-        Brand::factory(1)->create();
-        BrandCategory::factory(1)->create();
-        BrandInvoice::factory(1)->create();
-        BrandSocialLink::factory(1)->create();
-        Product::factory(20)->create();
-        ProductCategory::factory(20)->create();
-        ProductImage::factory(20)->create();
-        UserFavorite::factory(10)->create();
-        Gift::factory(10)->create();
-        UserGift::factory(2)->create();
-        GiftImage::factory(10)->create();
+//        Province::factory(1)->create();
+//        UserAddress::factory(10)->create();
+//        UserBillingAddress::factory(1)->create();
+//        UserOnline::factory(20)->create();
+//        UserPayment::factory(20)->create();
+//        UserPhone::factory(10)->create();
+//        UserProfile::factory(1)->create();
+//        UserProfilePicture::factory(1)->create();
+//        UserProvince::factory(1)->create();
+//        Category::factory(1)->create();
+//        Plan::factory(1)->create();
+//        RequestOpenBrand::factory(1)->create();
+//        RequestOpenBrandPayment::factory(1)->create();
+//        Brand::factory(1)->create();
+//        BrandCategory::factory(1)->create();
+//        BrandInvoice::factory(1)->create();
+//        BrandSocialLink::factory(1)->create();
+//        Product::factory(20)->create();
+//        ProductCategory::factory(20)->create();
+//        ProductImage::factory(20)->create();
+//        UserFavorite::factory(10)->create();
+//        Gift::factory(10)->create();
+//        UserGift::factory(2)->create();
+//        GiftImage::factory(10)->create();
     }
 }
